415c00b3caa4e4e19a55e67db1d4e633560550a2
Davi <85490673+Pr0wer@users.noreply.github.com>
2025-04-27 20:59:26 -0300

    Adição da funcionalidade da matriz e melhorias

--- Ohmimetro01.c
+++ Ohmimetro01.c
@@ -6,15 +6,12 @@
 #include "hardware/i2c.h"
 #include "lib/ssd1306.h"
 #include "lib/font.h"
-#define I2C_PORT i2c1
-#define I2C_SDA 14
-#define I2C_SCL 15
-#define endereco 0x3C
+#include "lib/ws2812b.h"
 #define ADC_PIN 28 // GPIO para o voltímetro
 #define Botao_A 5  // GPIO para botão A
 
 int R_conhecido = 10000;   // Resistor de 10k ohm
-float R_x = 9200.0;           // Resistor desconhecido
+float R_x = 73000.0f;           // Resistor desconhecido
 float ADC_VREF = 3.31;     // Tensão de referência do ADC
 int ADC_RESOLUTION = 4095; // Resolução do ADC (12 bits)
 
@@ -24,20 +21,34 @@ static const char *RESISTOR_COLORS[] = {
   "Laranja", "Amarelo", "Verde",
   "Azul", "Violeta", "Cinza", "Branco"
 };
+static const Rgb RESISTOR_RGB_COLOR[] = {
+  {0.0f, 0.0f, 0.0f}, // Preto
+  {2.0f, 1.5f, 0.0f}, // Marrom
+  {1.0f, 0.0f, 0.0f}, // Vermelho
+  {6.0f, 3.0f, 0.0f}, // Laranja
+  {1.0f, 1.0f, 0.0f}, // Amarelo
+  {0.0f, 1.0f, 0.0f}, // Verde
+  {0.0f, 0.0f, 1.0f}, // Azul
+  {0.5f, 0.15f, 1.0f}, // Violeta
+  {1.0f, 1.0f, 1.0f}, // Cinza
+  {10.0f, 10.0f, 10.0f} // Branco
+};
 
 #define RESISTOR_VALUES_COUNT 54
 static const float RESISTOR_VALUES[] = {
-  510.0, 560.0, 620.0, 680.0, 750.0, 820.0,
-  910.0, 1000.0, 1100.0, 1200.0, 1300.0, 1500.0,
-  1800.0, 2000.0, 2200.0, 2400.0, 2700.0, 3000.0,
-  3300.0, 3600.0, 3900.0, 4300.0, 4700.0, 5100.0,
-  5600.0, 6200.0, 6800.0, 7500.0, 8200.0, 9100.0,
-  10000.0, 11000.0, 12000.0, 13000.0, 15000.0,
-  18000.0, 20000.0, 22000.0, 24000.0, 27000.0,
-  30000.0, 33000.0, 36000.0, 39000.0, 43000.0,
-  47000.0, 51000.0, 56000.0, 62000.0, 68000.0,
-  75000.0, 82000.0, 91000.0, 100000.0
+  510.0f, 560.0f, 620.0f, 680.0f, 750.0f, 820.0f,
+  910.0f, 1000.0f, 1100.0f, 1200.0f, 1300.0f, 1500.0f,
+  1800.0f, 2000.0f, 2200.0f, 2400.0f, 2700.0f, 3000.0f,
+  3300.0f, 3600.0f, 3900.0f, 4300.0f, 4700.0f, 5100.0f,
+  5600.0f, 6200.0f, 6800.0f, 7500.0f, 8200.0f, 9100.0f,
+  10000.0f, 11000.0f, 12000.0f, 13000.0f, 15000.0f,
+  18000.0f, 20000.0f, 22000.0f, 24000.0f, 27000.0f,
+  30000.0f, 33000.0f, 36000.0f, 39000.0f, 43000.0f,
+  47000.0f, 51000.0f, 56000.0f, 62000.0f, 68000.0f,
+  75000.0f, 82000.0f, 91000.0f, 100000.0f
 };
+
+Rgb corLigamento = {1.0f, 1.0f, 1.0f};
 
 // Trecho para modo BOOTSEL com botão B
 #include "pico/bootrom.h"
@@ -65,21 +76,12 @@ int main()
   gpio_set_dir(Botao_A, GPIO_IN);
   gpio_pull_up(Botao_A);
 
-  // I2C Initialisation. Using it at 400Khz.
-  i2c_init(I2C_PORT, 400 * 1000);
+  // Inicializar matriz de LEDs
+  inicializarMatriz();
 
-  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-  gpio_pull_up(I2C_SDA);                                        // Pull up the data line
-  gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
-  ssd1306_t ssd;                                                // Inicializa a estrutura do display
-  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-  ssd1306_config(&ssd);                                         // Configura o display
-  ssd1306_send_data(&ssd);                                      // Envia os dados para o display
-
-  // Limpa o display. O display inicia com todos os pixels apagados.
-  ssd1306_fill(&ssd, false);
-  ssd1306_send_data(&ssd);
+  // Inicializar display SSD1306
+  ssd1306_t ssd;
+  ssd1306_i2c_init(&ssd);
 
   adc_init();
   adc_gpio_init(ADC_PIN); // GPIO 28 como entrada analógica
@@ -145,7 +147,6 @@ int main()
     sprintf(str_y, "%1.0f", R_x);   // Converte o float em string
     sprintf(str_z, "%1.0f", R_comercial); 
 
-    // cor = !cor;
     //  Atualiza o conteúdo do display
     ssd1306_fill(&ssd, !cor);                          // Limpa o display
     ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
@@ -153,14 +154,26 @@ int main()
     ssd1306_draw_string(&ssd, RESISTOR_COLORS[faixa_1], 8, 6);  // Desenha a cor da primeira faixa do resistor
     ssd1306_draw_string(&ssd, RESISTOR_COLORS[faixa_2], 8, 16);  // Desenha a cor da segunda faixa do resistor
     ssd1306_draw_string(&ssd, RESISTOR_COLORS[expMultiplicador], 8, 26);  // Desenha a cor da terceira faixa do resistor
-    ssd1306_draw_string(&ssd, "Comerc", 75, 16);  // Desenha a cor da primeira faixa do resistor
-    ssd1306_draw_string(&ssd, str_z, 75, 26);  // Desenha a cor da primeira faixa do resistor
+    ssd1306_draw_string(&ssd, "Comer", 77, 6);  // Desenha a cor da primeira faixa do resistor
+    ssd1306_draw_string(&ssd, "cial:", 77, 16);  // Desenha a cor da primeira faixa do resistor
+    ssd1306_draw_string(&ssd, str_z, 77, 26);  // Desenha a cor da primeira faixa do resistor
     ssd1306_draw_string(&ssd, "ADC", 13, 41);          // Desenha uma string
     ssd1306_draw_string(&ssd, "Resisten.", 50, 41);    // Desenha uma string
     ssd1306_line(&ssd, 44, 37, 44, 60, cor);           // Desenha uma linha vertical
+    ssd1306_line(&ssd, 73, 37, 73, 3, cor);           // Desenha uma linha vertical
     ssd1306_draw_string(&ssd, str_x, 8, 52);           // Desenha uma string
     ssd1306_draw_string(&ssd, str_y, 59, 52);          // Desenha uma string
     ssd1306_send_data(&ssd);                           // Atualiza o display
+
+    // Atualiza o conteúdo da matriz de LEDs
+    limparMatriz();
+    desenharColuna(1, 0, 3, RESISTOR_RGB_COLOR[faixa_1]);
+    desenharColuna(2, 1, 1, corLigamento);
+    desenharColuna(1, 2, 3, RESISTOR_RGB_COLOR[faixa_2]);
+    desenharColuna(2, 3, 1, corLigamento);
+    desenharColuna(1, 4, 3, RESISTOR_RGB_COLOR[expMultiplicador]);
+    atualizarMatriz();
+
     sleep_ms(700);
   }
 }
@@ -206,9 +219,9 @@ int16_t buscarValorProx(float valor)
   }
 
   // Comparar a diferença dos valores para retornar o mais próximo
-  if (abs(valor - RESISTOR_VALUES[primeiroMaior]) < abs(valor - RESISTOR_VALUES[primeiroMenor]))
+  if (abs(valor - RESISTOR_VALUES[primeiroMaior]) <= abs(valor - RESISTOR_VALUES[primeiroMenor]))
   {
-    return primeiroMaior; // O maior valor é o mais próximo
+    return primeiroMaior; // O maior valor é o mais próximo. Distâncias iguais se considera o maior valor
   }
   else
   {
--- lib/ssd1306.c
+++ lib/ssd1306.c
@@ -1,6 +1,27 @@
 #include "ssd1306.h"
 #include "font.h"
 
+void ssd1306_i2c_init(ssd1306_t *ssd)
+{
+    // Inicializar porta I2C
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); 
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); 
+
+    // Pull up da data e clock line
+    gpio_pull_up(I2C_SDA); 
+    gpio_pull_up(I2C_SCL); 
+
+    // Inicializar e configurar display
+    ssd1306_init(ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); 
+    ssd1306_config(ssd); 
+    ssd1306_send_data(ssd);
+  
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(ssd, false);
+    ssd1306_send_data(ssd);
+}
+
 void ssd1306_init(ssd1306_t *ssd, uint8_t width, uint8_t height, bool external_vcc, uint8_t address, i2c_inst_t *i2c) {
   ssd->width = width;
   ssd->height = height;
